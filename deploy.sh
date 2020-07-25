4764d2efd2c04515a6804c49f9be544d0492d04b
qazx7412 <git@newgenerations.im>
2020-07-25 18:09:42 +0900

    fix shard

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ stg=$1
 
 sudo docker run --rm -v $(pwd):/src -w /src      \
 crystallang/crystal crystal build                \
---link-flags -static -o bootstrap src/main.cr && \
+-static -o bootstrap src/main.cr              && \
 sudo chmod +x bootstrap                          || exit 1
 
 sls deploy -s $stg
